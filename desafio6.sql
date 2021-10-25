c7f05d1e2e0fc5522c8bf1c675f1359ebeeb8d6e
Murilo Ribeiro <murilorsv14@gmail.com>
2021-10-25 18:58:02 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,15 +1,15 @@
 SELECT
-CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo',
-a.JOB_TITLE AS Cargo,
-h.START_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS e
-LEFT JOIN hr.jobs AS a
-ON e.JOB_ID = a.JOB_ID
-RIGHT JOIN hr.job_history as h
-ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
-LEFT JOIN hr.departments as d
-ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+CONCAT(FIRST_NAME, ' ', LAST_NAME) 'Nome completo',
+JOB_TITLE AS Cargo,
+START_DATE AS 'Data de início do cargo',
+DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = jh.JOB_ID
+INNER JOIN hr.departments AS d
+ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
 
 -- SRC: utilizei o repositorio do Fernando para me ajudar a entender o que estava acontecendo https://github.com/tryber/sd-013-a-mysql-vocabulary-booster/pull/10/commits/a42c0ed5095bf7387061737582875931d937a864

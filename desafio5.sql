d5cee08070f1efa8809733c5d456d125338d8639
Murilo Ribeiro <murilorsv14@gmail.com>
2021-10-25 16:13:39 -0300

    desafio 4 e 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT
 JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-CAST(MIN_SALARY as DECIMAL(9,2)) / 12 AS 'Média mínima mensal',
-CAST(MAX_SALARY as DECIMAL(9,2)) / 12 AS 'Média Máxima mensal'
+CAST(MIN_SALARY / 12 AS DECIMAL(10, 2))  AS 'Média mínima mensal',
+CAST(MAX_SALARY / 12 AS DECIMAL(10, 2))  AS 'Média máxima mensal'
 FROM hr.jobs
 ORDER BY (MAX_SALARY - MIN_SALARY), Cargo;

8494585be354b9afae074525f44f08f9a9ae48ca
Murilo Ribeiro <murilorsv14@gmail.com>
2021-10-28 11:09:51 -0300

    desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo (IN job VARCHAR(45))
+BEGIN
+	SELECT	CAST(AVG(SALARY) AS DECIMAL(9,2)) AS "Média salarial"
+	FROM hr.jobs AS jobs
+	LEFT JOIN hr.employees AS employees
+	ON jobs.JOB_ID = employees.JOB_ID
+	WHERE JOB_TITLE = job
+    GROUP BY job;
+
+END $$
+
+DELIMITER ; 
+
+CALL buscar_media_por_cargo('Programmer');

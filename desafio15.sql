6cf70f8c8ecef528380ddc5c5afa9ba6a25adea7
Murilo Ribeiro <murilorsv14@gmail.com>
2021-10-28 11:12:53 -0300

    code climate

--- desafio15.sql
+++ desafio15.sql
@@ -2,11 +2,11 @@ USE hr
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo (IN job VARCHAR(45))
 BEGIN
-	SELECT	CAST(AVG(SALARY) AS DECIMAL(9,2)) AS "Média salarial"
-	FROM hr.jobs AS jobs
-	LEFT JOIN hr.employees AS employees
-	ON jobs.JOB_ID = employees.JOB_ID
-	WHERE JOB_TITLE = job
+  SELECT	CAST(AVG(SALARY) AS DECIMAL(9,2)) AS "Média salarial"
+  FROM hr.jobs AS jobs
+  LEFT JOIN hr.employees AS employees
+  ON jobs.JOB_ID = employees.JOB_ID
+  WHERE JOB_TITLE = job
     GROUP BY job;
 
 END $$
